42d34d5b9310703ce3b466e9e90dbb13d1953deb
4nnScript <annisa.nurhandayani@gmail.com>
2021-11-05 12:17:09 +0700

    uda ya

---
+++ src/landing.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';  
+import './App.css';
+import Footer from './components/footer/footer';
+import React from 'react';
+import Login from './components/login/login';
+import Navbar from './components/navbar/navbar';
+import Menu1 from './components/PMI/PMI_Menu _1'
+import { BrowserRouter as Router, Route, Redirect,Switch } from 'react-router-dom';
+
+
+
+// function App() {
+//   return ();
+// }
+
+// export default App;
